7611fd7bddb357d66a016aab744841d918ae6d40
doomer2000 <goose12313@gmail.com>
2025-10-01 12:37:47 +0400

    Add files via upload

---
+++ 1_17.py
@@ -0,0 +1,29 @@
+#print("Your name is:" , input("Enter your name:"))
+
+#print(input("Введите имя футб. команды: "), "– это чемпион!")
+
+#print("Привет, " , input("Введите свое имя: "), "!", sep='')
+
+#number = int(input("Введите число: "))
+#print("Следующее за числом" , number, "число -", number+1)
+#print("Для числа" , number, "предыдущее число -", number-1)
+
+#num1 = input("Введите число 1: ")
+#num2 = input("Введите число 2: ")
+#num3 = input("Введите число 3: ")
+#print(num1, num2, num3, sep='  ')
+
+#num1 = input("Введите число 1: ")
+#num2 = input("Введите число 2: ")
+#num3 = input("Введите число 3: ")
+#num4 = input("Введите число 4: ")
+#print(num1, num2, num3, num4)
+
+#varT = int(input("Введите t: "))
+#varV = int(input("Введите v: "))
+#varX = int(input("Введите x: "))
+#varY = int(input("Введите y: "))
+#
+#print("5" , "10", "\n", "7", "см")
+#print("10" , varT, "\n", "1949", varV)
+#print(varX , "25", "\n", varX, varY)
