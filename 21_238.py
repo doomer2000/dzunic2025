a6d2b63675175397579ca6a8f70ea1ef6b36d7fa
doomer2000 <goose12313@gmail.com>
2025-10-08 12:38:49 +0400

    Add files via upload

---
+++ 21_238.py
@@ -0,0 +1,123 @@
+import math
+#2.1a
+#funcX = float(input())
+#print(17*funcX**2-6*funcX+13)
+
+#2.1b
+#funcA = float(input())
+#print(3*funcA**2+5*funcA-21)
+
+#2.2
+#funcA = float(input())
+#print((funcA**2+10)/math.sqrt(funcA**2+1))
+
+#2.3a
+#funcA = float(input())
+#print(math.sqrt((2*funcA+math.sin(abs(3*funcA)))/3.56))
+
+#2.3b
+#funcX = float(input())
+#print(math.sin(3.2+math.sqrt(1+funcX)/abs(5*funcX)))
+
+#2.4
+#squareSide = float(input())
+#print(4*squareSide)
+
+#2.5
+#radius = float(input())
+#print(2*radius)
+
+#2.6
+#earthRadius = 6350
+#h = float(input())
+#print(math.sqrt(2*earthRadius*h))
+
+#2.7
+#lengthReber = float(input())
+#print(lengthReber**3)
+#print(4*lengthReber**2)
+
+#2.8
+#lengthReber = float(input())
+#print(lengthReber**3)
+#print(4*lengthReber**2)
+
+#2.9a
+#funcX = float(input())
+#funcY = float(input())
+#print(2*funcX**3-3.44*funcX*funcY+2.3*funcX**2-7.1*funcY+2)
+#2.9b
+#print(3.14*(funcX+funcY)**3+2.75*funcY**2-12.7*funcX-4.1)
+
+#2.10
+#num1 = float(input())
+#num2 = float(input())
+#print((num1+num2)/2)
+#print(math.sqrt(num1*num2))
+
+#2.11-2.12
+#varM = float(input())
+#varV = float(input())
+#print(varM/varV)
+
+#2.13
+#varA = float(input())
+#varB = float(input())
+#print(-(varA/varB))
+
+#2.14
+#varA = float(input())
+#varB = float(input())
+#print(math.sqrt(varA**2+varB**2))
+
+#2.15
+#varR = float(input())
+#varr = float(input())
+#print(math.pi*(varR**2-varr**2))
+
+#2.16
+#varA = float(input())
+#varB = float(input())
+#print(varA + varB + math.sqrt(varA**2+varB**2))
+
+#2.17
+#varA = float(input())
+#varB = float(input())
+#varH = float(input())
+#print(varA + varB + 2 * math.sqrt(((varA - varB) / 2) ** 2 + varH ** 2))
+
+#2.18
+#varX = float(input())
+#varY = float(input())
+#print(varX+((2+varY)/(varX**2)))/(varY+(1/(math.sqrt(varX**2 + 10))))
+#print(7.25*math.sin(varX-abs(varY)))
+
+#2.19
+#varA = float(input())
+#varB = float(input())
+#print((2/(varA**2 + 25))+varB)/(math.sqrt(varB)+((varA+varB)/2))
+#print(abs(varA)+2*math.sin(varB))/(5.5*varA)
+
+#2.20
+#varE = float(input())
+#varF = float(input())
+#varG = float(input())
+#varH = float(input())
+#print(math.sqrt((abs(varE-(3/varF)))**3 + varG))
+#print(math.sin(varE) + (math.cos(varH))**2)
+#print((33*varG)/(varE*varF -3))
+
+#2.21
+#varE = float(input())
+#varF = float(input())
+#varG = float(input())
+#varH = float(input())
+#print((varE+(varF/2))/3)
+#print(abs(varH**2 - varG))
+#print(math.sqrt((varG-varH)**2 - 3*math.sin(varE)))
+
+#2.22
+#num1 = abs(float(input()))
+#num2 = abs(float(input()))
+#print((num1+num2)/2)
+#print(math.sqrt(num1**2+num2**2))
